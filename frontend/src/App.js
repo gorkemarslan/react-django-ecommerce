c39dd4c34aa135fc7f173acbb17c035cbcc0ec61
grkm <garslan13@gmail.com>
2021-08-26 12:16:23 +0300

    Added Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
+      <Header />
+      <main>
+        
+      </main>
+      <Footer />
     </div>
   );
 }
